506c6ae4622c358f1f99f74c4f72643285ae43bf
amaan.saifi <amaan.saifi@goperceptive.com>
2022-12-27 23:08:27 +0530

    add templates & static files

--- blogposts/urls.py
+++ blogposts/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('about/', views.about),
     path('contact/', views.contact),
 ]
--- blogposts/views.py
+++ blogposts/views.py
@@ -5,7 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home page")
+    return render(request, 'blogposts/index.html')
+
+def login(request):
+    return render(request, 'blogposts/login.html')
+
+def register(request):
+    return render(request, 'blogposts/register.html')
+
+def about(request):
+    return render(request, 'blogposts/about.html')
 
 def contact(request):
-    return HttpResponse("Contact page")
+    return render(request, 'blogposts/contact.html')
+
